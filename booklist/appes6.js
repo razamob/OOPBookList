bbb8f42107168e39b5aa924a80e9971ab5ef9110
razamob <razamob@sheridancollege.ca>
2018-12-04 11:16:19 -0500

    Added data persistence

--- booklist/appes6.js
+++ booklist/appes6.js
@@ -45,6 +45,7 @@ class UI{
         if(target.className === 'delete'){
             target.parentElement.parentElement.remove();
         }
+
     }
 
     clearFields(){
@@ -54,6 +55,48 @@ class UI{
     }
 }
 
+//Local Storage Class
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }
+        else{
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+
+        return books;
+    }
+
+    static displayBooks(){
+        const books = Store.getBooks();
+        books.forEach(function(book){
+            const ui = new UI;
+            ui.addBookToList(book);
+        })
+    }
+
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book, index){
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+//DOM Load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
 //Event Listeners
 document.getElementById('book-form').addEventListener('submit',function(e){
     
@@ -75,6 +118,7 @@ document.getElementById('book-form').addEventListener('submit',function(e){
     else{
          //Add book to list
     ui.addBookToList(book);
+    Store.addBook(book);
     ui.showAlert('Booked Added!', 'success');
     ui.clearFields();
     }
@@ -86,6 +130,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
     const ui = new UI;
     ui.deleteBook(e.target);
+
+    //Remove from Local Storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent)
 
     //Show alert
     ui.showAlert('Book has been removed', 'success');
